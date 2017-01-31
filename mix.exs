d499ba8d50d5d84f35a0e5160c3742fb5d7b77b2
Jim Menard <jim.menard@chloeandisabel.com>
2017-01-31 17:01:18 -0500

    doc settings

--- mix.exs
+++ mix.exs
@@ -7,7 +7,15 @@ defmodule Napper.Mixfile do
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps()]
+     deps: deps(),
+
+     # for docs
+     name: "Napper",
+     source_url: "https://github.com/chloeandisabel/napper",
+     homepage_url: "https://github.com/chloeandisabel/napper",
+     docs: [main: "readme",
+            # logo: "",
+            extras: ["README.md"]]]
   end
 
   # Configuration for the OTP application
